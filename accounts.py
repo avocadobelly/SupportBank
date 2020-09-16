4841e258815e3237ad8eddb5e2ee3bf313d39e8f
gconnellbass <georgia.connell-bass@altran.com>
2020-09-16 12:06:43 +0100

    removed a print function. renamed variables of From and To accounts for clarity

--- accounts.py
+++ accounts.py
@@ -6,19 +6,18 @@ accounts = {}
 with open('Transactions2014.csv', newline='') as f:
     contents = csv.DictReader(f)
     for row in contents:
-        print(row)
-        From = row['From']
-        To = row['To']
-        if From in accounts:
-            from_account = accounts[From]
+        sender = row['From']
+        recipient = row['To']
+        if sender in accounts:
+            from_account = accounts[sender]
         else:
-            from_account = {'Holder': From, 'Balance': Decimal(0), 'Transaction' : []}
-            accounts[From] = from_account
-        if To in accounts:
-            to_account = accounts[To]
+            from_account = {'Holder': sender, 'Balance': Decimal(0), 'Transaction' : []}
+            accounts[sender] = from_account
+        if recipient in accounts:
+            to_account = accounts[recipient]
         else:
-            to_account = {'Holder': To, 'Balance': Decimal(0), 'Transaction' : []}
-            accounts[To] = to_account
+            to_account = {'Holder': recipient, 'Balance': Decimal(0), 'Transaction' : []}
+            accounts[recipient] = to_account
         transaction = row
         amount = Decimal(row['Amount'])
         from_account['Balance'] = from_account['Balance'] - amount
