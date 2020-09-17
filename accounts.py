eeafe02bd67e6293b6c16c0aa1082c421ce44ec5
gconnellbass <georgia.connell-bass@altran.com>
2020-09-17 15:58:43 +0100

    Added exception handling for date and decimal type conversions. Refactored some of the logging messages for added clarity. Removed some copy-paste errors

--- accounts.py
+++ accounts.py
@@ -13,59 +13,82 @@ class Transaction:
         self.narrative = narrative
         self.amount = amount
 
+
 class Account:
     def __init__(self,holder):
         self.holder = holder
         self.balance = Decimal(0)
         self.transaction = []
 
+
 accounts = {}
 with open('DodgyTransactions2015.csv', newline='') as f:
     logging.info('CSV file opened!')
     contents = csv.DictReader(f)
-    count = 0
+    row_count = 1
     for row in contents:
-        count = count + 1
-        logging.info('%d',count)
+        bad_row = False
+        row_count = row_count + 1
         unformatted_date = row['Date']
-        date = datetime.strptime(unformatted_date, '%d/%m/%Y').date()
-        logging.info('date formatted')
+        logging.info('unformatted_date: %s', unformatted_date)
+
+        try:
+            date = datetime.strptime(unformatted_date, '%d/%m/%Y').date()
+        except ValueError as e:
+            bad_row = True
+            print('Exception raised: %s. Conversion of %s to date format has failed. '
+                  'Please check if input supports this format.'% (e, unformatted_date))
+            date = None
+
         sender = row['From']
         logging.info('sender: %s', sender)
         recipient = row['To']
         logging.info('recipient: %s', recipient)
         narrative = row['Narrative']
         logging.info('narrative: %s', narrative)
-        amount = Decimal(row['Amount'])
+        unformatted_amount = row['Amount']
+        logging.info('unformatted_amount: %s', unformatted_amount)
+
+        try:
+            amount = Decimal(unformatted_amount)
+        except BaseException as e:
+            bad_row = True
+            print('Exception raised: %s. '
+                  'Type conversion of %s to decimal has failed.'
+                  ' Please check if input can be cast to a decimal'% (e, row['Amount']))
+            amount = None
 
         logging.info('amount: %s', amount)
 
         if sender in accounts:
             sender_account = accounts[sender]
-            logging.info('Sender name: %s recognised as belonging to one of the known accounts.', sender)
+            logging.info('Sender name of %s recognised as belonging to one of the known accounts.', sender)
         else:
-            sender_account = Account(holder = sender)
-            #assigns name to an account:
+            sender_account = Account(holder=sender)
             accounts[sender] = sender_account
-            logging.info('New account created for: %s', sender)
+            logging.info('New account created for %s', sender)
 
         if recipient in accounts:
-            reciever_account = accounts[recipient]
-            logging.info('Recipient name: %s recognised as belonging to one of the known accounts.', recipient)
+            receiver_account = accounts[recipient]
+            logging.info('Recipient name of %s recognised as belonging to one of the known accounts.', recipient)
         else:
-            reciever_account = Account(holder = recipient)
-            accounts[recipient] = reciever_account
-            logging.info('New account created for: %s', recipient)
+            receiver_account = Account(holder=recipient)
+            accounts[recipient] = receiver_account
+            logging.info('New account created for %s', recipient)
+
+        if bad_row:
+            print('Row %s in CSV file contains an incorrect data type'% row_count)
+            continue
 
         sender_account.balance = sender_account.balance - amount
-        logging.info('£%s leaves account of %s', (amount, sender_account))
-        reciever_account.balance = reciever_account.balance + amount
-        logging.info('£%s is sent to account of %s',(amount, reciever_account))
+        logging.info('£%s leaves account of %s', amount, sender_account.holder)
+        receiver_account.balance = receiver_account.balance + amount
+        logging.info('£%s is sent to account of %s', amount, receiver_account.holder)
         transaction = Transaction(date,sender,recipient,narrative,amount)
         sender_account.transaction.append(transaction)
-        logging.info('New transaction of £%s recorded in account of %s', (amount, sender_account))
-        reciever_account.transaction.append(transaction)
-        logging.info('New transaction of £%s recorded in account of %s', (amount, reciever_account))
+        logging.info('New transaction of £%s recorded in account of %s', amount, sender_account.holder)
+        receiver_account.transaction.append(transaction)
+        logging.info('New transaction of £%s recorded in account of %s', amount, receiver_account.holder)
 
 #List All
 #prints each Holder and the amount they owe or are owed
